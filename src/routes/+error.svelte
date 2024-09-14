07bbb1c8d5e2871b38a270b2549ec5397360c9d8
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2024-09-14 17:37:34 +0530

    basic 404 screen

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+	let errorMessage = 'And we have no idea what went wrong...';
+	if ($page.status == 404) {
+		errorMessage = "You're not supposed to be here!";
+	}
+</script>
+
+<h1>Something went wrong!</h1>
+<p>{errorMessage}</p>
